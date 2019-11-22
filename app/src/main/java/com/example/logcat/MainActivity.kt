71ea53bf89e9cdb67436b2a9e46d771bcdeb1aa5
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2019-11-22 23:45:28 +0530

    comments

--- app/src/main/java/com/example/logcat/MainActivity.kt
+++ app/src/main/java/com/example/logcat/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.logcat
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +14,7 @@ class MainActivity : AppCompatActivity() {
 
        btnShowToast.setOnClickListener {
            Log.i("MainActivity","Button was clicked")
+           Toast.makeTest(this,"Button was clicked", Toast.LENGTH_SHORT).show()
        }
 
 
