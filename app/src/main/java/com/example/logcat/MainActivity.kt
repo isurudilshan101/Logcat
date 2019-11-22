24e361145dd85289a2edea81fbef9a712e04b742
isurudilshan101 <40594182+isurudilshan101@users.noreply.github.com>
2019-11-22 23:17:02 +0530

    comments 2

--- app/src/main/java/com/example/logcat/MainActivity.kt
+++ app/src/main/java/com/example/logcat/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.logcat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+       btnShowToast.setOnClickListener {
+           Log.i("MainActivity","Button was clicked")
+       }
+
+
     }
 }
